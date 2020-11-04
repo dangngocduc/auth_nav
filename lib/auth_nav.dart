6b5b9020ce52e36f00654c52ccd1a426ca09faa9
dangngocduc <dangngocduc.bk@gmail.com>
2020-11-04 08:42:07 +0700

    refactor export lib

--- lib/auth_nav.dart
+++ lib/auth_nav.dart
@@ -1,7 +1,6 @@
 library auth_nav;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'navigation/auth_navigation.dart';
+export 'navigation/splash_page.dart';
+export 'bloc/auth_bloc.dart';
+export 'bloc/auth_navigation_state.dart';
